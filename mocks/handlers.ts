217da21a5ad245b0a419963377ba8ddfcf5a2a7d
김용민 <dydals3440@gmail.com>
2024-04-25 04:12:29 +0900

    chore: Initial Setting

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,6 +1,15 @@
 import { http, HttpResponse } from 'msw';
 
+const User = [{ id: 'dydals3440', nickname: 'HIHIHI' }];
+
 export const handlers = [
+	http.post('/api/login', () => {
+		return HttpResponse.json(User[1], {
+			headers: {
+				'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+			},
+		});
+	}),
 	http.get('/api/posts', () => {
 		return HttpResponse.json([
 			{
